ac5fcffeb279ded9887ae56a332ecb40d1e6dca7
David McCarthy <cartog73@yahoo.com>
2013-07-12 20:23:09 +0000

    My edits and associated errors

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
-var express = require('express');
 
+
+var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var indexbuf = fs.readFileSync("index.html"); //or try reading with option "utf8"
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexbuf.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
